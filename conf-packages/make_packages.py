125fa687d286de3c8440919a301bfbf7298d9645
Joao Mesquita <jmesquita@indicium.com.ar>
2015-04-14 08:56:32 -0300

    Modification to make possible to build other package versions

--- conf-packages/make_packages.py
+++ conf-packages/make_packages.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python 
 
+from debian import changelog
+import sys
 import subprocess
 import os 
 
@@ -9,7 +11,7 @@ def list_my_dirs():
             continue
         yield d
 
-def build(distro, arch):
+def build(distro, arch, package=None):
     f = '/var/cache/pbuilder/base-%s-%s.cow' % (distro, arch)
     if not os.path.exists(f):
         print 'Generating cowbuilder for building packages.'
@@ -19,11 +21,18 @@ def build(distro, arch):
     subprocess.check_call(['cowbuilder', '--update', '--distribution', distro, '--architecture', arch, '--basepath', f])
     for d in list_my_dirs():
         print 'Generating packages for %s' % d
+	version = '0.1'
+	with open(os.path.join(d, 'debian/changelog')) as changefile:
+		c = changelog.Changelog(changefile)
+		version = '%s' % c.get_version()
         os.chdir(d)
         subprocess.check_call(['dpkg-source', '-b', '.'])
         os.chdir('../')
-        subprocess.check_call(['cowbuilder', '--build', '%s_0.1.dsc' % (d), '--distribution', distro, '--architecture', arch, '--basepath', f, '--buildresult', '.'])
+        subprocess.check_call(['cowbuilder', '--build', '%s_%s.dsc' % (d, version.replace('-', '_')), '--distribution', distro, '--architecture', arch, '--basepath', f, '--buildresult', '.'])
 
 if __name__ == "__main__":
-    build('wheezy', 'amd64')
+    if len(sys.argv) > 2:
+        build('wheezy', 'amd64', sys.argv[1])
+    else:
+        build('wheezy', 'amd64')
     # build('wheezy', 'i386')
